35442f9ae36f53aca3f9e8393e34ad0aec70033c
KSLW <kirk3497@gmail.com>
2025-11-21 14:17:46 +0000

    Updated base route to show that backend is running

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 // Health check
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
