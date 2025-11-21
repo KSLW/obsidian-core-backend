11c3343988f97cdff8587984beda57cf351cc676
KSLW <kirk3497@gmail.com>
2025-11-21 09:55:53 +0000

    Updated auth.routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,26 +1,21 @@
+// backend/routes/auth.routes.js
 const express = require("express");
 const router = express.Router();
 const {
   getSettingsController,
-  updateSettingsController,
   saveTwitchKeys,
   twitchLogin,
   twitchCallback,
   resetAuth,
 } = require("../controllers/auth.controller");
 
-// Settings
 router.get("/settings", getSettingsController);
-router.put("/settings", updateSettingsController);
 
-// Twitch app keys
 router.post("/auth/twitch/keys", saveTwitchKeys);
 
-// Twitch OAuth
 router.get("/auth/twitch/login", twitchLogin);
 router.get("/auth/twitch/callback", twitchCallback);
 
-// Reset auth tokens
 router.post("/auth/reset/:provider", resetAuth);
 
 module.exports = router;
